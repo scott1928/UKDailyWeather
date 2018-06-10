126159faa2ee7e27f52bc2944a83c8583d1c8c3b
scott1928 <scott1928@gmail.com>
2018-06-10 13:33:25 +0100

    small code changes

--- version0.py
+++ version0.py
@@ -2,9 +2,9 @@ import config
 import requests
 import random
 
-##city_list = ['Vienna','New York','Barcelona','Rio de Janeiro','Bangkok','Sydney','Dubrovnik','Cape Town','Los Angeles','Orlando','Mexico City','Athens','Istanbul','Tel Aviv','Moscow']
-
-
+##city_list = ['Vienna','New York','BARCELONA','Rio De Janeiro','Bangkok','Sydney','Dubrovnik','Cape Town','Los Angeles','Orlando','Mexico City','Athens','Istanbul','Tel Aviv','Moscow','ABU DHABI','Amsterdam','Ankara','BAGHDAD',
+##'BAKU','Beijing','Belgrade','Berlin','Budapest','Buenos Aires','Cairo','CARACAS','Copenhagen','DAMASCUS','Dhaka','Gibralta','GUATAMALA','Havana','Hong Kong','JAKARATA','KABUL','Kathmandu','Kiev','Kuala Lumper','KUWAIT','Lisbon',
+##'MADRID','Manila','Monaco','NAIROBI','New Delhi','Singapore','TOKYO','Zagreb']
 ##uk_city_list = ['Brighton','London','Liverpool','Edinburgh','Leicester','Plymouth']
 
 
@@ -31,7 +31,6 @@ def uk_high_temp():
 def world_temps():
 	i = 0
 	uk_temp_run = uk_high_temp()
-	uk_temp_run[1]
 	colder_citys = []
 	colder_temps = []
 	for city in config.world_citys:
@@ -56,7 +55,7 @@ def world_temps():
 
 word_gen = [["Great Britain ","Britain ","The UK ","The British Isles ","The United Kingdom "] #0
 ,["SWEATS ", "is MELTING ", "BOILS ", "SWELTERS ", "IS BLAZING ", "SCORCHES ","SIZZLES ","BAKES ","IS TROPICAL ","IS RED HOT "] #1
-,["in the sun as the temperature ", "as the sun shines down and it ", "as the mercury ","as the suns rays beat down and it ","as clouds clear and it "] #2
+,["in the sun as the temperature ", "as the sun shines down and it ", "as the mercury ","as the sun's rays beat down and it ","as clouds clear and it "] #2
 ,["climbs HIGHER than ","raises ABOVE ", "INCREASES to ", "BREAKS through to "] #3
 ,["making us HOTTER than ", "BEATING even ", "causing us to SWEAT more than ", "putting us on course to be WARMER than "] #4
 ,[" which ONLY has a measley ", " which ONLY reaches ", " which BARELY hits ", " which is a MERE ", " which CAN'T even get above "] #5
@@ -74,8 +73,8 @@ def headline_generator():
 	index = word_generator()
 	uk_temp = uk_high_temp()
 	world_temps_gen = world_temps()
-	return (word_gen[0][index[0]]) + word_gen[1][index[1]] + word_gen[2][index[2]] + word_gen[3][index[3]] + str(uk_temp[1]) + str(" degrees ") + word_gen[4][index[4]] + str(world_temps_gen[0]) + word_gen[5][index[5]] + str(world_temps_gen[1]) + " degrees while " + word_gen[6][index[6]] + word_gen[7][index[7]] + " #dailyweather #ukweather #hot #uk"
+	return (word_gen[0][index[0]]) + word_gen[1][index[1]] + word_gen[2][index[2]] + word_gen[3][index[3]] + str(uk_temp[1]) + str(" degrees ") + word_gen[4][index[4]] + str(world_temps_gen[0]) + word_gen[5][index[5]] + str(world_temps_gen[1]) + " degrees while " + word_gen[6][index[6]] + word_gen[7][index[7]] + " #dailyweather #UKweather #hot #uk #dailyexpress #dailymail"
 
 
-print(headline_generator())
+
 
