e8a7e37becbe3397217b3be095d350565def671a
scott1928 <scott1928@gmail.com>
2018-07-02 09:44:49 +0100

    added a internet connection check to avoid a dropped connection missing a tweet

--- ___init___.py
+++ ___init___.py
@@ -3,6 +3,7 @@ import random
 import version0
 import os
 import image_picker
+import time
 
 from auth import (
     consumer_key,
@@ -17,6 +18,29 @@ twitter = Twython(
     access_token_secret
 )
 
+try:
+    import httplib
+except:
+    import http.client as httplib
+
+def have_internet():
+    conn = httplib.HTTPConnection("www.google.com", timeout=5)
+    try:
+        conn.request("HEAD", "/")
+        conn.close()
+        return True
+    except:
+        conn.close()
+        return False
+
+
+internet = have_internet()
+print("Internet online")
+
+while internet == False:
+	print("internet offline at " + time.strftime("%H:%M,%S"))
+	time.sleep(1200)
+	internet = have_internet()
 
 imgfile = image_picker.img_pick()
 message = version0.headline_generator() 
