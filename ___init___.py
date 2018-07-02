da462d45bdd07c449df19fc86d49447191cae607
scott1928 <scott1928@gmail.com>
2018-07-02 10:31:08 +0100

    changed the folder selection to use config instead of a local directory

--- ___init___.py
+++ ___init___.py
@@ -4,6 +4,7 @@ import version0
 import os
 import image_picker
 import time
+import config
 
 from auth import (
     consumer_key,
@@ -44,7 +45,7 @@ while internet == False:
 
 imgfile = image_picker.img_pick()
 message = version0.headline_generator() 
-photo = open('/Users/scottkirwan/Documents/api/pics/'+str(imgfile),'rb')
+photo = open(config.folder+str(imgfile),'rb')
 response = twitter.upload_media(media=photo)
 twitter.update_status(status=message,media_ids=[response['media_id']])
 print("Tweeted: {}".format(message))
